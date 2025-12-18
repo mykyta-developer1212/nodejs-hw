fd4d2d7e75ffa895922c6e7502b094335251c118
Mykyta <nikita.borodin1212@gmail.com>
2025-12-18 20:57:31 +0200

    commit

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,24 +1,21 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUserSchema, loginUserSchema, } from '../validations/authValidation.js';
-import { registerUser, loginUser, refreshUserSession, logoutUser, } from '../controllers/authController.js';
+import { registerUserSchema, loginUserSchema } from '../validations/authValidation.js';
+import {
+  registerUser,
+  loginUser,
+  refreshUserSession,
+  logoutUser,
+} from '../controllers/authController.js';
 
 const router = Router();
 
-router.post('/auth/register', celebrate(registerUserSchema), registerUser);
-router.post('/auth/login', celebrate(loginUserSchema), loginUser);
-router.post('/auth/refresh', refreshUserSession);
-router.post('/auth/logout', logoutUser);
+router.post('/register', celebrate(registerUserSchema), registerUser);
+router.post('/login', celebrate(loginUserSchema), loginUser);
+router.post('/refresh', refreshUserSession);
+router.post('/logout', logoutUser);
 
 export default router;
-
-
-
-
-
-
-
-
 
 
 
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { errors } from "celebrate";
 
 import { connectMongoDB } from "./db/connectMongoDB.js";
 import notesRouter from "./routes/notesRoutes.js";
+import authRouter from "./routes/authRoutes.js";
 import { logger } from "./middleware/logger.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import { errorHandler } from "./middleware/errorHandler.js";
@@ -15,20 +17,24 @@ const PORT = process.env.PORT || 3000;
 app.use(logger);
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
-app.use(notesRouter);
+app.use("/auth", authRouter);
+app.use("/notes", notesRouter);
 
 app.use(notFoundHandler);
-
 app.use(errors());
-
 app.use(errorHandler);
 
 const startServer = async () => {
-  await connectMongoDB();
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  try {
+    await connectMongoDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Startup error:", error);
+  }
 };
 
 startServer();
